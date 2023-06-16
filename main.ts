9859dbf97d51dbf9149ac061e20d9b4228a451b4
Eliaz Bobadilla <ultirequiem@pm.me>
2023-06-16 16:14:13 -0500

    first commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+const numbersUntil = (until: number) =>
+  Array.from({ length: until }, (_, index) => index);
+
+const numbersUntilX = numbersUntil(10100000);
+
+const nope = [2, 3, 7, 8];
+
+let found = false;
+
+for (const num of numbersUntilX) {
+  const numPowTwo = num ** 2;
+  const lastDigitNumPowTwo = numPowTwo.toString().slice(-1);
+
+  if (nope.includes(+lastDigitNumPowTwo)) {
+    console.log(`Eureka: ${num} (${numPowTwo})`);
+    found = true;
+  }
+}
+
+if (found) {
+  console.log(
+    `Congratulations, you found a number raised to the square that ends in 2,3,7 or 8.`
+  );
+} else {
+  console.log(
+    `You didn't find any number raised to the square that ends in 2,3,7 or 8.`
+  );
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# The numbers raised to the square never end 2,3,7 or 8
+
+You can run the bunle version of this script in your browser's console.
+
+> [Run JavaScript in the Console](https://developer.chrome.com/docs/devtools/console/javascript)
+
+Todo: Create math prove paper
+
+https://deopurkar.github.io/teaching/algebra1/cheng.pdf
